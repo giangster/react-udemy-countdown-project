3653d13efc330a7ae7e666e251cfdbcc3bea6d22
Giang Nguyen <thuygiang152@gmail.com>
2019-03-13 16:00:23 +0200

    final ver

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     days: 0,
     hours: 0,
     minutes: 0,
-    seconds: 0
+    seconds: 0,
+    countable: true
   };
 
   inputHandler = event => {
@@ -17,12 +18,23 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
+  countDownNow = event => {
     setInterval(() => this.countDown(this.state.deadline), 1000);
+    this.setState({
+      countable: true
+    });
+  };
+
+  leading0(num) {
+    return num < 10 ? "0" + num : num;
   }
 
   countDown(date) {
     var distance = Date.parse(date) - Date.parse(new Date());
+    if (distance < 0) {
+      clearInterval(this.countDownNow);
+      this.setState({ countable: false });
+    }
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor(
       (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -41,25 +53,47 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-          <h2>Let's count down to {this.state.deadline} &#9752;</h2>
+          <h1>Let's count down to {this.state.deadline} &#9752;</h1>
           <img
             src="https://img.jakpost.net/c/2016/08/18/2016_08_18_9979_1471497895._large.jpg"
-            style={{ width: 300, height: 200, margin: 20 }}
+            style={{ width: 350, height: 250, margin: 20 }}
             alt="your birthday cake this year..."
           />
-          <div>
-            <span>Only </span>
-            <div className="count-days">{this.state.days} days</div>
-            <div className="count-hours">{this.state.hours} hours</div>
-            <div className="count-minutes">{this.state.minutes} minutes</div>
-            <div className="count-seconds">{this.state.seconds} seconds</div>
-            <span> till your birthday!</span>
-          </div>
+          {this.state.countable ? (
+            <div>
+              <span>Only </span>
+              <div className="count-days">
+                {this.leading0(this.state.days)} days
+              </div>
+              <div className="count-hours">
+                {this.leading0(this.state.hours)} hours
+              </div>
+              <div className="count-minutes">
+                {this.leading0(this.state.minutes)} minutes
+              </div>
+              <div className="count-seconds">
+                {this.leading0(this.state.seconds)} seconds
+              </div>
+              <span> till your birthday!</span>
+            </div>
+          ) : (
+            <div>
+              OOPS! You already had your birthday this year! How about counting
+              down to the next one?{" "}
+              <span
+                role="img"
+                aria-label="SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES"
+              >
+                &#128518;
+              </span>
+            </div>
+          )}
           <input
             placeholder="enter new date"
             type="date"
             onChange={this.inputHandler}
           />
+          <button onClick={this.countDownNow}>Let's count!</button>
         </div>
       </div>
     );
